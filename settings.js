33339acf78e40c5d87952da59690536e5291c0ad
KING-BOT-OFFICIAL <94338947+nethsaragimhan@users.noreply.github.com>
2022-08-06 14:31:50 +0530

    Add files via upload

--- settings.js
+++ settings.js
@@ -8,78 +8,25 @@ global.APIs = {
 
 //Api Key\\
 global.APIKeys = {
-	'https://zenzapis.xyz': 'your key',
+	'https://zenzapis.xyz': 'sanzychan01', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
 }
 
-
-//auto functioner
-global.autoTyping = false //auto tying in gc (true to on, false to off)
-global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
-global.autoReadGc = false //auto reading in gc (true to on, false to off)
-global.autoRecord = false //auto recording (true to on, false to off)
-global.available = true //auto available (true to on, false to off)
-
-//semt menu variants
-global.vidmenu = fs.readFileSync("./XeonMedia/theme/Cheems-bot.mp4") //gif and video menu
-
-//documents variants
-global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
-global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-global.doc4 = 'application/zip'
-global.doc5 = 'application/pdf'
-global.doc6 = 'application/vnd.android.package-archive'
-
-//owmner v card
-global.vcardowner = ['94729352830'] //ur owner number
-global.ownername = "ШHłТΞ HΛϾКΞЯS" //ur owner name
-global.ytname = "YT: Tech With A.N.G." //ur yt chanel name
-global.socialm = "GitHub: KING-BOT-OFFICIAL" //ur github or insta name
-global.location = "Sri Lanka" //ur location
-
-//bot bomdy 
+//customize settings\\
 global.owner = ['94729352830']
-global.ownertag = '94729352830' //ur tag number
-global.botname = '[🇱🇰𝚱𝚰𝚴Ｇ 𝛃𝚯𝚪🤘]' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = "https://github.com/KING-BOT-OFFICIAL/KING-BOT-MD" //script link
-global.reactmoji = "🔥" //ur menu react emoji
-global.themeemoji = "🔥" //ur theme emoji
-global.packname = "[🇱🇰𝚱𝚰𝚴Ｇ 𝛃𝚯𝚪🤘]" //ur sticker watermark packname
-global.author = "ШHłТΞ HΛϾКΞЯS" //ur sticker watermark author
-
-//Bot theme media
-global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
-global.log0 = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur logo pic
-global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
-global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
-
-//damtabase
-global.premium = ['94729352830'] //ur premium numbers
-global.ntilinkytvid = []
-global.ntilinkytch = []
-global.ntilinkig = []
-global.ntilinkfb = []
-global.ntilinktg = []
-global.ntilinktt = []
-global.ntilinktwt = []
-global.ntilinkall = []
-global.nticall = []
-global.ntwame = []
-global.nttoxic = []
-global.ntnsfw = []
-global.ntvirtex = []
-global.rkyt = []
-global.wlcm = []
-global.gcrevoke = []
-global.autorep = []
-global.ntilink = []
-
-//omther
+global.premium = ['94729352830']
+global.ownernomer = '94729352830'
+global.ownername = 'ШHłТΞ HΛϾКΞЯS'
+global.botname = '[🇱🇰𝚱𝚰𝚴Ｇ 𝛃𝚯𝚪🤘]'
+global.footer = '</> ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋɪɴɢ ʙᴏᴛ </>️ ▷'
+global.ig = 'https://github.com/KING-BOT-OFFICIAL'
+global.sc = 'https://github.com/KING-BOT-OFFICIAL/KING-BOT-MD'
+global.myweb = 'https://youtube.com/channel/UCgwWV1Cya4_gUFKYOQYQtHw'
+global.packname = '[🇱🇰𝚱𝚰𝚴Ｇ 𝛃𝚯𝚪🤘]'
+global.author = 'ШHłТΞ HΛϾКΞЯS'
 global.sessionName = 'session'
-global.antitags = true
-global.prefa = ['','!','.','🔥','🎭','🤘']
+global.region = 'Sri Lanka'
+global.reactmoji = '🎭'
+global.prefa = ['','!','.','🇱🇰','⚙️','🤘']
 global.sp = '⭔'
 global.mess = {
     success: 'Done ✓',
@@ -90,12 +37,8 @@ global.mess = {
     private: 'Features Used Only For Private Chat!',
     bot: 'This Feature Is Only For Bot',
     wait: '⏳ ᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ ᴀ ᴍᴏᴍᴇɴᴛ...',
-    linkm: 'Where is the link?',
-    error: 'Error!!',
+    error: 'Error! Maybe Api Key Is Expired🤔!',
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
-    ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
-    nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
-    banChat: 'The bot was banned in this group, please contact the owner to unban'
 }
     global.limitawal = {
     premium: "Infinity",
@@ -110,7 +53,7 @@ global.mess = {
    umpanawal: 5,
    potionawal: 1
 }
-global.thumb = fs.readFileSync('./src/logo.jpeg')
+global.thumb = fs.readFileSync('./KINGMedia/logo.jpeg')
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
