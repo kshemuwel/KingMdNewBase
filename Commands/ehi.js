d1fd1fa0d1a8a88916f1a0729b9b3f08e11bb7f8
KING-BOT-OFFICIAL <94338947+nethsaragimhan@users.noreply.github.com>
2022-08-06 15:04:05 +0530

    addedehipack

--- Commands/ehi.js
+++ Commands/ehi.js
@@ -7,6 +7,15 @@ const axios = require('axios')
 const path = require('path')
 const os = require('os')
 
+
+    var body = (m.mtype === 'conversation') ? m.message.conversation : (m.mtype == 'imageMessage') ? m.message.imageMessage.caption : (m.mtype == 'videoMessage') ? m.message.videoMessage.caption : (m.mtype == 'extendedTextMessage') ? m.message.extendedTextMessage.text : (m.mtype == 'buttonsResponseMessage') ? m.message.buttonsResponseMessage.selectedButtonId : (m.mtype == 'listResponseMessage') ? m.message.listResponseMessage.singleSelectReply.selectedRowId : (m.mtype == 'templateButtonReplyMessage') ? m.message.templateButtonReplyMessage.selectedId : (m.mtype === 'messageContextInfo') ? (m.message.buttonsResponseMessage?.selectedButtonId || m.message.listResponseMessage?.singleSelectReply.selectedRowId || m.text) : ''
+    var budy = (typeof m.text == 'string' ? m.text : '')
+    var prefix = prefa ? /^[°•π÷×¶∆£¢€¥®™+✓_=|~!?@#$%^&.©^]/gi.test(body) ? body.match(/^[°•π÷×¶∆£¢€¥®™+✓_=|~!?@#$%^&.©^]/gi)[0] : "" : prefa ?? global.prefix
+    const isCmd = body.startsWith(prefix)
+    const command = body.replace(prefix, '').trim().split(/ +/).shift().toLowerCase()
+
+
+
 switch(command) {
             case 'ehi': {
             KingmdWH.sendMessage(from, { react: { text: "🛡️", key: m.key }})
